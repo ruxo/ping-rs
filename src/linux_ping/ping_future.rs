d79800019bc524819efef6fcec0851c157086df4
Ruxo Zheng <rz@just.sent.as>
2023-01-04 09:04:21 +0700

    Handle error in async poll

--- src/linux_ping/ping_future.rs
+++ src/linux_ping/ping_future.rs
@@ -1,25 +1,24 @@
 use std::future::Future;
-use std::os::fd::{AsRawFd, RawFd};
+use std::os::fd::{AsRawFd};
 use std::pin::Pin;
-use std::sync::mpsc::{Receiver, sync_channel, SyncSender};
-use std::sync::RwLock;
-use std::sync::atomic::AtomicBool;
+use std::sync::{Arc, RwLock};
 use std::task::{Context, Poll, Waker};
-use std::thread;
+use std::{io, mem, thread};
+use std::os::raw::c_int;
 use mio::{Events, Interest, Token};
 use mio::unix::SourceFd;
 use socket2::Socket;
-use crate::linux_ping::PingContext;
+use crate::linux_ping::{get_timestamp, PingContext};
 use crate::{PingApiOutput, PingError};
 
-pub(crate) struct RawFdSocket(Socket);
 pub(crate) struct PingFuture {
     context: PingContext,
+    error: Arc<RwLock<Option<io::Error>>>
 }
 
 impl PingFuture {
     pub(crate) fn new(context: PingContext) -> Self {
-        Self { context }
+        Self { context, error: Arc::new(RwLock::new(None)) }
     }
 }
 
@@ -31,8 +30,16 @@ impl Future for PingFuture {
         match reply {
             Err(PingError::IoPending) => {
                 println!("waiting..");
-                start_poller(&self.context.socket, cx.waker().clone());
+                start_poller(&self.context.socket, cx.waker().clone(), self.error.clone());
                 Poll::Pending
+            },
+            Ok(_) => {
+                let mut error_wrapper = self.error.write().unwrap();
+                let error = mem::replace(&mut *error_wrapper, None);
+                Poll::Ready(match error {
+                    None => reply,
+                    Some(e) => Err(e.into())
+                })
             },
             _ => Poll::Ready(reply)
         }
@@ -40,30 +47,22 @@ impl Future for PingFuture {
 }
 
 // INTERNAL
-static WAKER_QUEUE: RwLock<Option<SyncSender<Waker>>> = RwLock::new(None);
-static PING_POLLER: AtomicBool = AtomicBool::new(false);
+const DUMMY_TOKEN: Token = Token(123);
 
-fn start_poller(socket: &Socket, waker: Waker) {
-    /*
-    let (sender, receiver) = sync_channel(64);
-    let mut queue = WAKER_QUEUE.write().unwrap();
-    *queue = Some(sender.clone());
-
-     */
+fn start_poller(socket: &Socket, waker: Waker, error: Arc<RwLock<Option<io::Error>>>) {
     let fd = socket.as_raw_fd();
-    thread::spawn(move || {
-        // TODO handle error properly
 
+    fn poll(fd: c_int, waker: Waker) -> io::Result<()> {
         println!("start polling");
-        let mut poll = mio::Poll::new().unwrap();
+        let mut poll = mio::Poll::new()?;
         let mut events = Events::with_capacity(64);
 
-        poll.registry().register(&mut SourceFd(&fd), Token(123), Interest::READABLE).unwrap();
-        poll.poll(&mut events, None).unwrap();
+        poll.registry().register(&mut SourceFd(&fd), DUMMY_TOKEN, Interest::READABLE)?;
+        poll.poll(&mut events, None)?;
 
         for event in &events {
             match event.token() {
-                Token(123) => {
+                DUMMY_TOKEN => {
                     println!("awakened!");
                     waker.clone().wake();
                 },
@@ -71,6 +70,15 @@ fn start_poller(socket: &Socket, waker: Waker) {
             }
         }
         println!("end thread");
+        Ok(())
+    }
+
+    thread::spawn(move || {
+        let result = poll(fd, waker).err();
+        match error.write() {
+            Ok(mut e) => *e = result,
+            Err(_) => panic!("impossible")
+        }
     });
     // sender
 }
